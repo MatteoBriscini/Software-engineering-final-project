0fc9daa9b88f85f9aeac1ac6efb98a19086b1b3a
Matteo Briscini Polimi <126170808+MatteoBriscini@users.noreply.github.com>
2023-03-16 13:09:54 +0100

    restart2

--- server_ids/src/main/java/it/polimi/ingsw/GroupTargets/GT10.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.GroupTargets;
-
-public class GT10 extends GroupTarget{
-
-    public void check(){};
-
-}
--- server_ids/src/main/java/it/polimi/ingsw/GroupTargets/GT3.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.GroupTargets;
-
-public class GT3 extends GroupTarget{
-
-    public void check(){};
-
-}
