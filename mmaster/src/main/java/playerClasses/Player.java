0cc8a9cbcbf39db971a2349b4460119e8129b0a3
DavideArcainiPoliMi <126171511+DavideArcainiPoliMi@users.noreply.github.com>
2023-03-16 10:50:55 +0100

    Add files via upload

---
+++ mmaster/src/main/java/playerClasses/Player.java
@@ -0,0 +1,25 @@
+package playerClasses;
+
+public class Player {
+
+    //Attributes
+
+    private String playerID;
+    private int pointSum;
+    //private int pointArray;
+    private playerBoard board;
+    private playerTarget personalTarget;
+
+
+    //Constructor
+
+    public player(String playerID, playerBoard board, playerTarget personalTarget){
+
+        this.playerID = playerID;
+        this.board = board;
+        this.personalTarget = personalTarget;
+        pointSum = 0;
+
+    }
+
+}
