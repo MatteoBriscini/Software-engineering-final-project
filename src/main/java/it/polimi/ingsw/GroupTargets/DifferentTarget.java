0249caa0d08a9ed840796aa2b874acc6a6605cdd
LucaCastelliPolimi <luca6.castelli@mail.polimi.it>
2023-03-20 13:07:37 +0100

    Update class EqualTarget and DifferentTarget

--- src/main/java/it/polimi/ingsw/GroupTargets/DifferentTarget.java
+++ src/main/java/it/polimi/ingsw/GroupTargets/DifferentTarget.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class DifferentTarget extends GroupTarget{
-    private boolean different(Card[] cards) {
+    public boolean different(Card[] cards) {
         HashSet<Card> hs = new HashSet<>(Arrays.asList(cards));
         if(hs.size() == cards.length) {return true;}
         else {return false;}
--- src/main/java/it/polimi/ingsw/GroupTargets/EqualTarget.java
+++ src/main/java/it/polimi/ingsw/GroupTargets/EqualTarget.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class EqualTarget extends GroupTarget{
-    private boolean allEqual (Card[] cards) {
+    public boolean allEqual (Card[] cards) {
         HashSet<Card> hs = new HashSet<>(Arrays.asList(cards));
         if(hs.size() == 1 ) {return true;}
         else {return false;}
