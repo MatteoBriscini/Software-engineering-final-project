f5ed61dbf7b5379d7f6a8ef84c2090b07516aa28
DavideArcainiPoliMi <davide1.arcaini@mail.polimi.it>
2023-03-21 10:31:05 +0100

    building Login class

--- src/main/java/it/polimi/ingsw/Lobby.java
+++ src/main/java/it/polimi/ingsw/Lobby.java
@@ -1,98 +1,91 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
+
 public class Lobby {
-    private List<Player> players;
-    private Player creator;
-    private boolean gameStarted;
+    private int[][] activeGame; // matrix with ID of active game in first column and players in remaining columns
+    private ArrayList<Player> allPlayers; // list of all players
 
-    // Constructor
     public Lobby() {
-        this.players = new ArrayList<Player>();
-        this.creator = null;
-        this.gameStarted = false;
+        activeGame = new int[10][5]; // assume 10 active games with up to 4 players each (random choice)
+        allPlayers = new ArrayList<Player>;
     }
 
-
-
-    // Method to add a player to the lobby
-    public void addPlayer(Player player) {
-        if (!gameStarted && !players.contains(player)) { // can only add players if game has not started and player is not already in the lobby
-            players.add(player);
-            if (creator == null) { // assign power to create, end and invite to the first player who joins
-                creator = player;
-                creator.setPowerToCreateGame(true);
-            }
-        }
-    }
-
-    // Method to remove a player from the lobby
-    public void removePlayer(int playerId) {
-        if (!gameStarted) { // can only remove players if game has not started
-            for (Player player : players) {
-                if (player.getId() == playerId) {
-                    players.remove(player);
-                    if (player.equals(creator)) { // if the creator leaves, assign the power to create to the next player who joined
-                        if (!players.isEmpty()) {
-                            creator = players.get(0);
-                            creator.setPowerToCreateGame(true);
-                        } else {
-                            creator = null;
+    // login method to check the username and password and add player to active game if possible
+    public boolean login(String username, String password) {
+        for (Player p : allPlayers) {
+            if (p.getUsername().equals(username) && p.getPassword().equals(password)) {
+                // check if player is already in an active game
+                for (int i = 0; i < activeGame.length; i++) {
+                    for (int j = 0; j < activeGame[i].length; j++) {
+                        if (activeGame[i][j] != 0 && activeGame[i][j] == p.getID()) {
+                            // player is in an active game, add them back to it
+                            return true;
                         }
                     }
-                    break;
+                }
+                // player is not in an active game, allow login
+                return true;
+            }
+        }
+        // 404 player not found or password incorrect
+        return false;
+    }
+
+    // registration method to add new player to allPlayers list
+    public boolean registration(String username, String password) {
+        // check if username already exists
+        for (Player p : allPlayers) {
+            if (p.getUsername().equals(username)) {
+                return false;
+            }
+        }
+        // add new player to list
+        allPlayers.add(new Player(username, password, allPlayers.size() + 1));
+        return true;
+    }
+
+    // create game method to add new game to activeGame matrix
+    public boolean createGame(int ownerID) {
+        // find first empty row in matrix
+        for (int i = 0; i < activeGame.length; i++) {
+            if (activeGame[i][0] == 0) {
+                activeGame[i][0] = ownerID; // set owner ID in first column (is the payer with god power)
+                return true;
+            }
+        }
+        // no empty rows found
+        return false;
+    }
+
+    // end game method to remove game from activeGame matrix
+    public boolean endGame(int gameID, int ownerID) {
+        // find game with matching ID and owner
+        for (int i = 0; i < activeGame.length; i++) {
+            if (activeGame[i][0] == gameID && activeGame[i][1] == ownerID) {
+                activeGame[i] = new int[6]; // clear row
+                return true;
+            }
+        }
+        // game not found or owner ID incorrect
+        return false;
+    }
+
+    // join game method to add player to existing game
+    public boolean joinGame(int gameID, int playerID) {
+        // find game with matching ID and room for player
+        for (int i = 0; i < activeGame.length; i++) {
+            if (activeGame[i][0] == gameID) {
+                for (int j = 1; j < activeGame[i].length; j++) {
+                    if (activeGame[i][j] == 0) {
+                        activeGame[i][j] = playerID;
+                        return true;
+                    }
                 }
             }
         }
-    }
-
-    // Method to start the game
-    public void startGame() {
-        if (!gameStarted && creator != null) { // can only start game if no game is already started and a creator has been assigned
-            gameStarted = true;
-            // Do something to start the game...
-        }
-    }
-
-    // Method to end the game
-    public void endGame() {
-        if (gameStarted) { // can only end game if game has started
-            gameStarted = false;
-            // Do something to end the game...
-        }
-    }
-
-
-    public static void main(String[] args) {
-        // Create a lobby
-        Lobby lobby = new Lobby();
-
-        // still need to understand how made other players join with and without invitation, so I create them atm
-        Player alice = new Player(1, "Alice", false);
-        Player bob = new Player(2, "Bob", false);
-        Player charlie = new Player(3, "Charlie", false);
-        Player dave = new Player(4, "Dave", true); // Dave is the first to join, so he has the power to create games
-
-        // Add players to the lobby
-        lobby.addPlayer(dave);
-        lobby.addPlayer(alice);
-        lobby.addPlayer(bob);
-        lobby.addPlayer(charlie);
-
-        // Start the game (should not work because no player has power to create game)
-        lobby.startGame();
-
-        // Assign the power to create game to Bob
-        bob.setPowerToCreateGame(true);
-
-        // Start the game (should work now)
-        lobby.startGame();
-
-        // End the game
-        lobby.endGame();
-
-        // Remove Bob from the lobby and assign power to create the game to Alice
-        lobby.removePlayer(bob);
-        alice.setPowerToCreateGame(true);
+        // game not found or no room for player
+        return false;
     }
 }
 
