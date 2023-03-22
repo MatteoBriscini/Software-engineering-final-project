4c770839435002be02b2167c47cd6af10c50de86
riccardo <riccardo.caprioglio@mail.polimi.it>
2023-03-22 15:15:23 +0100

    PlayerBoard updated and correction errors

--- src/main/java/it/polimi/ingsw/PlayerClasses/PlayerBoard.java
+++ src/main/java/it/polimi/ingsw/PlayerClasses/PlayerBoard.java
@@ -18,7 +18,7 @@ public class PlayerBoard {
         board = new Card[5][6];
         for (int i = 0; i < 5; i++){
             for (int j = 0; j < 6; j++){
-                board[i][j] = new Card(EMPTY, 0);
+                board[i][j] = new Card(EMPTY);
             }
         }
     }
@@ -32,23 +32,34 @@ public class PlayerBoard {
 
     //Methods
 
-    public void addCard(int column, Card[] cards){
+    public boolean addCard (int column, Card[] cards) throws NoSpaceException{
 
-        int i = 5, j = 0;
+        int i = 5, j = 0, flag = 0;
 
+        for(int k = 0; k < 4; k++){
+            if(board[k][i].getColor().equals(EMPTY)){
+                flag = 1;
+            }
+        }
 
+        if(flag != 1){
+            return true;
+        }
 
-        while(board[column][i].getColor().equals(EMPTY)){
+        while(board[column][i].getColor().equals(EMPTY) && i > 0){
             i--;
         }
 
         while(cards[j] != null && j < 3){
 
             i++;
+            if(i > 5){ throw new NoSpaceException("Full column"); }
             board[column][i] = cards[j];
             j++;
 
         }
+
+        return false;
 
     }
 
