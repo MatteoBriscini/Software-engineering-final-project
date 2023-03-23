fba6901c3ff36143f1f6af977156548ba8669c7c
matteo <matteo.briscini@mail.polimi.it>
2023-03-24 00:18:16 +0100

    CrossTarget completely tested and implemented

--- src/main/java/it/polimi/ingsw/GroupTargets/EqualTarget.java
+++ src/main/java/it/polimi/ingsw/GroupTargets/EqualTarget.java
@@ -6,7 +6,11 @@ import static it.polimi.ingsw.Cards.CardColor.EMPTY;
 import java.util.Arrays;
 import java.util.HashSet;
 
-public class EqualTarget extends GroupTarget{
+public class EqualTarget extends CommonGoal {
+    /**
+     * @param cards array of cards to compare
+     * @return boolean true if all the array is equal
+     */
     public boolean allEqual (Card[] cards) {
         HashSet<Card> hs = new HashSet<>(Arrays.asList(cards));          //create hash set with same element of the array
         for (Card c : hs){                                              //verify there are no empty card
