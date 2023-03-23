fe027513189f656c2b69032b485115ae8c912822
DavideArcainiPoliMi <davide1.arcaini@mail.polimi.it>
2023-03-23 12:29:18 +0100

    class PlayerTest finished

--- src/test/java/it/polimi/ingsw/PlayerClasses/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/PlayerClasses/PlayerTest.java
@@ -413,4 +413,154 @@ public class PlayerTest extends TestCase {
 
 
 
+
+    //check with empty board (test 4) or with a full board with a single color (test 5)
+
+    public void testCheckSpots4(){
+
+        System.out.println("START TEST 4\n");
+        int i, j;
+
+        //test case 4 (check in initial case if sum = 0 )
+        player.checkSpots();
+        int sum;
+        sum = player.getPointSum();
+        assert (sum == 0);
+
+
+        //print library
+        board=playerBoard.getBoard();
+        for(int y=5;y>=0;y--){
+            System.out.println(board[0][y].getColor().toString()+" "+board[1][y].getColor().toString()+" "+board[2][y].getColor().toString()+" "+board[3][y].getColor().toString()+" "+board[4][y].getColor().toString()+" ");
+        }
+        System.out.println("\n");
+
+        player.checkSpots();
+        System.out.println(player.getPointSum());
+        assert (player.getPointSum() == 0);
+
+        System.out.println("end test 4");
+
+    }
+
+    public void testCheckSpots5(){
+
+        System.out.println("START TEST 5\n");
+        int i, j;
+
+        //test case 4 (check in initial case if sum = 0 )
+        player.checkSpots();
+        int sum;
+        sum = player.getPointSum();
+        assert (sum == 0);
+
+        //FILLING LIBRARY
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(0, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards1");
+        }
+
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(0, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards2");
+        }
+
+
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(1, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards3");
+        }
+
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(1, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards4");
+        }
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(2, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards5");
+        }
+
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(2, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards6");
+        }
+
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(3, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards7");
+        }
+
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(3, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards8");
+        }
+
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(4, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards9");
+        }
+
+        tmp[0] = new Card(BLUE);
+        tmp[1] = new Card(BLUE);
+        tmp[2] = new Card(BLUE);
+        try {
+            player.addCard(4, tmp);
+        } catch (NoSpaceException e) {
+            System.out.println("too much cards10");
+        }
+
+
+        //print library
+        board=playerBoard.getBoard();
+        for(int y=5;y>=0;y--){
+            System.out.println(board[0][y].getColor().toString()+" "+board[1][y].getColor().toString()+" "+board[2][y].getColor().toString()+" "+board[3][y].getColor().toString()+" "+board[4][y].getColor().toString()+" ");
+        }
+        System.out.println("\n");
+
+        player.checkSpots();
+        System.out.println(player.getPointSum());
+        assert (player.getPointSum() == 8);
+
+        System.out.println("end test 5");
+
+    }
+
+
+
 }
