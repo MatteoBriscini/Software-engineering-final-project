f8513e49521ea4b4c451c022bd638638eba43ab8
DavideArcainiPoliMi <davide1.arcaini@mail.polimi.it>
2023-03-28 13:45:21 +0200

    GameMaster

--- src/main/java/it/polimi/ingsw/GameMaster.java
+++ src/main/java/it/polimi/ingsw/GameMaster.java
@@ -1,15 +1,18 @@
 package it.polimi.ingsw;
 
 import it.polimi.ingsw.Cards.Card;
+import it.polimi.ingsw.Exceptions.LenghtException;
 import it.polimi.ingsw.Exceptions.NoSpaceException;
 import it.polimi.ingsw.JsonSupportClasses.Position;
 import it.polimi.ingsw.JsonSupportClasses.PositionWithColor;
 import it.polimi.ingsw.PlayerClasses.*;
 import it.polimi.ingsw.GroupTargets.*;
+
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class GameMaster {
-    private ArrayList<Player> players;
+    private ArrayList<Player> players = new ArrayList<>();
     private CommonGoal[] commonGoals;
     private final MainBoard mainBoard = new MainBoard();
 
@@ -20,10 +23,10 @@ public class GameMaster {
     public ArrayList<Player> getPlayerArray(){
         return players;
     }
-/*
+
     public Card[][] getMainBoard() {
         return mainBoard.getBoard();
-    }*/
+    }
 
     public PlayerTarget getPlayerGoal(int currentPlayer){
         return players.get(currentPlayer).getPersonalTarget();
@@ -41,42 +44,74 @@ public class GameMaster {
         return players.size();
     }
     public void setCommonGoal (int commonGoalID){//da fare **************************************************
+        switch (commonGoalID) {
 
+        }
     }
 
-    public void setPrivateGoal (int[] privateGoalID){//da fare **************************************************
+    /**
+     * @param privateGoalID is an array that contains identifier of the common goals
+     * @throws FileNotFoundException exception if the private goals are not enough for the players
+     * @throws LenghtException exception if the private goals are not enough for the players
+     */
+    public void setPrivateGoal (int[] privateGoalID) throws FileNotFoundException, LenghtException {
+
+        // Check that the number of private goals matches the number of players
+        if (privateGoalID.length != players.size()){
+            throw new LenghtException("different length for arrays privateGaolId or players");
+        }
+
+        //Iterate over players and assign the private goal
+         for (int i = 0; i < players.size(); i++) {
+             players.get(i).setPlayerTarget(privateGoalID[i]);  //call the method in Player
+         }
     }
 
+    /**
+     * @param validPosition are the correct positions for the player number
+     * @return return the board filled
+     */
     public boolean fillMainBoard(Position[] validPosition){
         return mainBoard.fillBoard(validPosition);
     }
 
     /**
-     * point method
+     * this method calculate the points scored at the end of the game
      */
-    public int endGameCalcPoint(){ //da fare
-        return 0;
+    public void endGameCalcPoint(int currentPlayer){ //return the total scored by the player
+        players.get(currentPlayer).checkSpots();
+        players.get(currentPlayer).checkPlayerTarget();
+
     }
+
+    /** mathod to add points to the players
+     * @param point points to add
+     * @param currentPlayer player that need the points added
+     */
     public void playerAddPoint(int point, int currentPlayer){
             players.get(currentPlayer).updatePointSum(point);
     }
 
+    /**
+     * @param currentPlayer the player from who I get the points
+     * @return give back the player points
+     */
     public int playerGetPoint(int currentPlayer){
         return players.get(currentPlayer).getPointSum();
     }
 
     /**
-     * Card method
+     * Card methods, add or remove a card from the board
      */
     public boolean addCard(int column, Card[] cards, int currentPlayer) throws NoSpaceException {  //call a try-catch on addCard in player
         return players.get(currentPlayer).addCard(column, cards);
     }
-    public boolean removeCards(PositionWithColor[] cards){ //da fare **************************************************
-        return false;
+    public boolean removeCards(PositionWithColor[] cards){  //need a boolean in mainBoard
+        return mainBoard.removeCard(cards);
     }
 
     /**
-     * commonGoals method
+     * commonGoals method, give info about the commonGoals, if it was already scored, if the common goal was scored in this turn and if it does, set that as already scored
      */
     public ArrayList<Player> getAlreadyScored (int commonGoalID){
         return commonGoals[commonGoalID].getAlreadyScored();
@@ -84,7 +119,7 @@ public class GameMaster {
     public void setAlreadyScored(ArrayList<Player> alreadyScored, int commonGoalID){
         commonGoals[commonGoalID].setAlreadyScored(alreadyScored);
     }
-    public boolean checkCommonGoal(int commonGoalID, int currentPlayer){   //have to take current player board
+    public boolean checkCommonGoal(int commonGoalID, int currentPlayer){    //have to take current player board
         return commonGoals[commonGoalID].check(players.get(currentPlayer).getBoard());
     }
 
