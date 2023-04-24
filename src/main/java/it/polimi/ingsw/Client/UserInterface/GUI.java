153b2cf8548ef9de74fd37afb62657ccbe969106
DavideArcainiPoliMi <davide1.arcaini@mail.polimi.it>
2023-04-24 19:15:55 +0200

    javaFX

--- src/main/java/it/polimi/ingsw/Client/UserInterface/GUI.java
+++ src/main/java/it/polimi/ingsw/Client/UserInterface/GUI.java
@@ -1,7 +1,7 @@
-package it.polimi.ingsw.client.UserInterface;
+package it.polimi.ingsw.Client.UserInterface;
 
 public class GUI {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
     }
 }
