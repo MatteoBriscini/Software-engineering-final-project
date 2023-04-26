e7f880fff64fe14073a250dc0602064d0eeb3a9a
riccardo <riccardo.caprioglio@mail.polimi.it>
2023-04-26 15:46:51 +0200

    PlayerBoard client copy

--- src/main/java/it/polimi/ingsw/Client/Game/PlayerBoard.java
+++ src/main/java/it/polimi/ingsw/Client/Game/PlayerBoard.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.Client.Game;
 
 import it.polimi.ingsw.Shared.Cards.Card;
+import java.util.ArrayList;
 
 import static it.polimi.ingsw.Shared.Cards.CardColor.*;
 
@@ -14,12 +15,31 @@ public class PlayerBoard {
 
     public PlayerBoard(Card[][] board){
 
+        this.board = new Card[x][y];
+        for (int i = 0; i < x; i++){
+            for (int j = 0; j < y; j++){
+                this.board[i][j] = board[i][j];
+            }
+        }
+
     }
 
     public boolean checkSpace(int column, int cards){
 
         if(board[column][y-cards].getColor() == EMPTY){return true;}
         return false;
+
+    }
+
+    public void addCard(int column, ArrayList<Card> cards){
+
+        int j = 0;
+
+        for(int i = cards.size(); i > 0; i++){
+            board[column][y-i] = cards.get(j);
+            j++;
+        }
+
     }
 
 }
