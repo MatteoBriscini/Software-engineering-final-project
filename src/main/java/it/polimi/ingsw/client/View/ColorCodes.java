e6063e4f5265c99e097a49700891f89efd606922
LucaCastelliPolimi <luca6.castelli@mail.polimi.it>
2023-05-07 10:49:24 +0200

    Fix last commit

---
+++ src/main/java/it/polimi/ingsw/client/View/ColorCodes.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.client.View;
+
+public enum ColorCodes {
+
+    BLUE("\033[44m"),   // BLUE
+    GREEN("\033[42m"),  // GREEN
+    LIGHTBLUE("\033[46m"),   // CYAN
+    PURPLE("\033[45m"), // PURPLE
+    WHITE("\033[47m"),  // WHITE
+    YELLOW("\033[43m"), // YELLOW
+    EMPTY("\033[40m"),  // BLACK
+    RED_BACKGROUND("\033[41m");    // RED
+
+
+
+
+
+
+    private String code;
+
+
+    public String get() {
+        return code;
+    }
+
+    public void set(String code){
+        this.code=code;
+    }
+
+    ColorCodes(String code){
+        this.code=code;
+    }
+}
