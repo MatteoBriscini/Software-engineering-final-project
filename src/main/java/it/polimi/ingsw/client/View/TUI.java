5c88c3a33214762f5923dfdf060355ea8d33bd26
LucaCastelliPolimi <luca6.castelli@mail.polimi.it>
2023-05-10 11:25:29 +0200

    Add takeCard method (only partially implemented)
    Commands for the selection of the type of connection and login/signup are no more case-sensitive
    
    Add javadoc for all the methods,except for methods takeCard (javadoc will be added when the method is finished) and toRun (this method will be replaced)

--- src/main/java/it/polimi/ingsw/client/View/TUI.java
+++ src/main/java/it/polimi/ingsw/client/View/TUI.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.client.View;
 import it.polimi.ingsw.client.Connection.ClientRMI;
 import it.polimi.ingsw.client.Connection.ClientSOCKET;
 import it.polimi.ingsw.client.Connection.ConnectionManager;
+import it.polimi.ingsw.client.Exceptions.PlayerNotFoundException;
 import it.polimi.ingsw.client.Game.MainBoard;
 import it.polimi.ingsw.client.Game.PlayerBoard;
 import it.polimi.ingsw.client.Player.LobbyPlayer;
@@ -11,6 +12,7 @@ import it.polimi.ingsw.client.Player.PlayingPlayer;
 import it.polimi.ingsw.shared.Cards.Card;
 import it.polimi.ingsw.shared.Cards.CardColor;
 import it.polimi.ingsw.shared.Connection.ConnectionType;
+import it.polimi.ingsw.shared.JsonSupportClasses.Position;
 
 import java.net.Socket;
 import java.util.*;
@@ -39,34 +41,39 @@ public class TUI {
         userIdentification();
     }
 
+    /**
+     * this method allows the player to choose how to connect to the server (via Socket or RMI)
+     */
     public void connectionSelection(){
 
-        Scanner sc = new Scanner(System.in);
         char c;
 
         do {
             System.out.println("Select the type of the connection:\n[R] RMI\n[S] Socket");
-            c = sc.next().charAt(0);
+            c = charCommand();
             if(c!='R' && c!='S')
-                System.out.println("Invalid selection, please try again\n");
+                printError("Invalid selection, please try again\n");
         }while(c!='R' && c!='S');
 
         if(c=='S'){
             try {
                 connection = new ClientSOCKET(1234,"127.0.0.1");
             } catch (Exception e) {
-                System.out.println("Server is offline");
+                player.disconnectError("Server is offline");
             }
         }
         else{
             try {
                 connection = new ClientRMI(1234,"127.0.0.1");
             } catch (Exception e) {
-                System.out.println("Server is offline");
+                player.disconnectError("Server is offline");
             }
         }
     }
 
+    /**
+     * this method allows the player to log in or sign up
+     */
     public void userIdentification() {
 
         Scanner sc = new Scanner(System.in);
@@ -81,15 +88,16 @@ public class TUI {
             if(user.equals("/back")) {
                 do {
                     System.out.println("What do you want to do?\n[S]Sign up\n[L]Log in");
-                    selection = sc.next().charAt(0);
-                    if (selection != 'T' && selection != 'G')
-                        System.out.println("Invalid selection, please try again\n");
+                    selection = charCommand();
+                    if (selection != 'S' && selection != 'L')
+                        printError("Invalid selection, please try again\n");
                 } while (selection != 'S' && selection != 'L');
             }
+
             logged=false;
             System.out.println("Enter your username (or /back to return to the previous selection):");
             user=sc.nextLine();
-            if(!user.equals("/back")) {
+            if(!(user.equals("/back"))) {
                 System.out.println("Enter your password:");
                 pwd = sc.nextLine();
 
@@ -150,33 +158,139 @@ public class TUI {
     }
 
 
+    /**
+     * this method allows the player to join create a new game
+     */
     public void createGame(){
         Scanner sc = new Scanner(System.in);
         String selection;
+        boolean success;
 
+        success=false;
         do {
             System.out.println("Insert the number of players for this game (min 2, max 4)\nInsert /def to use default settings");
             selection = sc.nextLine();
+            selection.toLowerCase();
             if (selection.equals("/def")) {
-                ((LobbyPlayer)player).createGame();
+                success=((LobbyPlayer)player).createGame();
+            } else if (Integer.parseInt(selection)>=2 && Integer.parseInt(selection)<=4) {
+                success=((LobbyPlayer)player).createGame(Integer.parseInt(selection));
             }
-            if(Integer.parseInt(selection)>=2 && Integer.parseInt(selection)<=4)
-                ((LobbyPlayer)player).createGame(Integer.parseInt(selection));
-            else System.out.println("Invalid selection, please try again");
-        }while(Integer.parseInt(selection)<2 || Integer.parseInt(selection)>4);
+        }while(!success);
+    }
 
+    /**
+     * this method allows the player to join an existent game
+     */
+    public void joinGame(){
+        Scanner sc = new Scanner(System.in);
+        String selection;
+        boolean success;
 
+        success=false;
+        do{
+            System.out.println("Insert the name of a player to play with a friend, or /rand to join a random game");
+            selection = sc.nextLine();
+            if(selection.equals("/rand")){
+                success=((LobbyPlayer)player).joinGame();
+            } else {
+                success=((LobbyPlayer)player).joinGame(selection);
+            }
+        }while (!success);
 
 
     }
 
 
+    /**
+     * this method reads the message from the standard input stream and calls the function on the player to send the message
+     * if the message is directed to a not existent player, an error is printed on the standard error stream
+     */
     public void sendMsg(){
         Scanner sc = new Scanner(System.in);
-        //((PlayingPlayer)player).sendMsg(sc.nextLine());
+        try {
+            ((PlayingPlayer) player).sendMessage(sc.nextLine());
+        } catch (PlayerNotFoundException e){
+            printError("CHAT ERROR: " + e.getMessage());
+        }
     }
 
+
+    /**
+     * this method prints a message sent to the player
+     * @param msg is the string that contains the message sent to the player
+     */
     public void receiveMsg(String msg){
         System.out.println(msg);
+    }
+
+
+    /**
+     * this method calls the method on the player to start a new game
+     */
+    public void startGame(){
+        ((PlayingPlayer)player).startGame();
+    }
+
+    public void takeCard(){
+        Scanner sc = new Scanner(System.in);
+        String selection;
+        char sel;
+        String[] picks;
+        String[] tmpSel;
+        String[] singlePick;
+        Position[] positions;
+        String tmpPos;
+
+        do {
+            System.out.println("Select the positions of the cards you want to take: x1,y1; [...]; xn,yn\n");
+            selection = sc.nextLine();
+            picks = selection.split(";");
+        } while (picks.length<1 || picks.length>3);
+
+        positions = new Position[picks.length];
+        for(int i=0; i< picks.length;i++){
+            singlePick=picks[i].split(",");
+            positions[i]= new Position(Integer.parseInt(singlePick[0]),Integer.parseInt(singlePick[1]));
+        }
+
+        /** stampare carte selezionate */
+
+        do {
+            System.out.println("Do you want to change the order of the cards? Y/N");
+            sel =charCommand();
+        }while (sel!='Y' && sel!='N');
+
+        if(sel=='Y'){
+            do{
+                System.out.println("Insert the new order, from the bottom to the top: bottom card, [...], top card\n");
+                selection = sc.nextLine();
+                tmpSel=selection.split(",");
+
+                if(tmpSel.length==picks.length){
+                    for(int i=0;i<picks.length;i++) {
+                        tmpPos = picks[i];
+                        picks[i]=picks[Integer.parseInt(tmpSel[i])];
+                        picks[Integer.parseInt(tmpSel[i])]=tmpPos;
+                    }
+                }
+                else printError("The number of tiles you want to reorder is different from the number of tiles you picked");
+
+            }while(tmpSel.length!=picks.length);
+        }
+    }
+
+
+    /**
+     * @param err is the string to print on the standard error stream
+     */
+    public void printError(String err){
+        System.err.println(err);
+    }
+
+
+    public char charCommand(){
+        Scanner sc = new Scanner(System.in);
+        return(Character.toUpperCase(sc.next().charAt(0)));
     }
 }
