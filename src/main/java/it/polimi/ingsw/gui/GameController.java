e21e59ed51157be9cf641ec241be5616e7619a93
DavideArcainiPoliMi <davide1.arcaini@mail.polimi.it>
2023-05-20 16:55:52 +0200

    building waitingRoom

--- src/main/java/it/polimi/ingsw/gui/GameController.java
+++ src/main/java/it/polimi/ingsw/gui/GameController.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.client.Connection.ConnectionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import com.google.gson.Gson;
@@ -29,7 +30,61 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class GameController extends GuiView{
+public class GameController extends GuiView implements Initializable {
 
+
+    PlayingPlayer player;
+
+    @FXML
+    private ImageView myBookshelfImage = new ImageView();
+
+    @FXML
+    private ImageView myBookshelfImage1 = new ImageView();
+    @FXML
+    private ImageView myBookshelfImage2 = new ImageView();
+    @FXML
+    private ImageView myBookshelfImage3 = new ImageView();
+    @FXML
+    private ImageView livingRoom = new ImageView();
+    @FXML
+    private ImageView personalGoal1 = new ImageView();
+
+
+    @FXML
+    private ImageView commonGoal1 = new ImageView();
+    @FXML
+    private ImageView commonGoal2 = new ImageView();
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+       // player = (PlayingPlayer) helloApplication.getPlayer();
+
+        Image image = new Image(this.getClass().getClassLoader().getResourceAsStream("bookshelf.png"));
+        myBookshelfImage.setImage(image);
+
+        myBookshelfImage1.setImage(image);
+
+        myBookshelfImage2.setImage(image);
+
+        myBookshelfImage3.setImage(image);  //load the other boards
+
+        Image image1 = new Image(this.getClass().getClassLoader().getResourceAsStream("livingroom.png"));
+        livingRoom.setImage(image1);
+
+        Image image2 = new Image(this.getClass().getClassLoader().getResourceAsStream("1.jpg"));
+        commonGoal1.setImage(image2);
+
+        Image image3 = new Image(this.getClass().getClassLoader().getResourceAsStream("2.jpg"));
+        commonGoal2.setImage(image3);
+
+
+
+
+        Image image4 = new Image(this.getClass().getClassLoader().getResourceAsStream("Personal_goals.png"));
+        personalGoal1.setImage(image4);
+
+
+    }
 
 }
